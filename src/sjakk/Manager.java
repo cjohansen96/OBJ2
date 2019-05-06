4d175820ef5b1975b0f89c689eadbcbb6a49e3c2
Markus W.Byremo <markuswb77@gmail.com>
2019-05-06 19:45:01 +0200

    Gjort metode utskiftninger mellom Manager og Turngering

--- src/sjakk/Manager.java
+++ src/sjakk/Manager.java
@@ -14,35 +14,26 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class Manager {
-    private List<Parti> partier;
+    
 
+    private static final String RANGERING_FIL = "rangering.dat";
     private static final String TURNERING_FIL = "turnering.dat";
 
     ObjectOutputStream outputStream = null;
     ObjectInputStream inputStream = null;
     
     public Manager() {
-        partier = new ArrayList<>();
-    }
-    
-    public ObservableList<Parti> getPartier() {
-        lastInnFil();
-        ObservableList<Parti> partiListe = FXCollections.observableArrayList(partier);
         
-       
-        return partiListe;
-    }  
-    
-    public void leggTilParti(Spiller spiller1, Spiller spiller2, String dato, String klokkeSlett, String vinner, String trekk) {
-        lastInnFil();
-        partier.add(new Parti(spiller1, spiller2, dato, klokkeSlett, vinner, trekk));
-        oppdaterFil();
     }
     
-    public void lastInnFil() {
+    
+  
+    public ArrayList<Parti> lastInnParti() {
         try {
             inputStream = new ObjectInputStream(new FileInputStream(TURNERING_FIL));
-            partier = (List<Parti>) inputStream.readObject();
+            ArrayList<Parti> list = (ArrayList<Parti>)inputStream.readObject();
+            
+            return list;
             
             
 
@@ -62,12 +53,13 @@ public class Manager {
                 System.out.println("[Last inn] IO Error: " + e.getMessage());
             }
         }
+        return null;
     }
     
-    public void oppdaterFil() {
+    public void oppdaterParti(Parti nyttParti) {
         try {
             outputStream = new ObjectOutputStream(new FileOutputStream(TURNERING_FIL));
-            outputStream.writeObject(partier);
+            outputStream.writeObject(nyttParti);
 
         } catch (FileNotFoundException e) {
             System.out.println("[Oppdatering] FNF Error: " + e.getMessage() + ", programmet lager ny fil!");
@@ -76,7 +68,7 @@ public class Manager {
         } finally {
             try {
                 if (outputStream != null) {
-                    outputStream.flush();
+                    outputStream.flush(); 
                     outputStream.close();
                 }
             } catch (IOException e) {
@@ -85,48 +77,58 @@ public class Manager {
         }
     }
     
-    public String getPartierUtTekst() {
-        String partiString = "";
-
-        List<Parti> parti;
-        parti = getPartier();
-
-        int i = 0;
-        for(Parti p: parti) {
-            partiString +=  parti.get(i).getDato() + " " + parti.get(i).getKlokkeSlett() + " "  
-                 + parti.get(i).getSpiller1().getNavn() + " " + parti.get(i).getSpiller2().getNavn() + " " 
-                 + parti.get(i).getResultat() + " " + parti.get(i).getTrekk() + ";" + "\n";
-            i++;
-        }
-        return partiString;
-    }
-    
-    
-      public static ObservableList<Spiller> getSpillere() {
+  
+      public ArrayList<Spiller> LastInnSpillere() {
         
-        ObservableList<Spiller> spillere = FXCollections.observableArrayList();
-        
-        File file = new File("src/sjakk/DummyRank.txt");
-        try(Scanner sc = new Scanner(file);){
+            try {
+            inputStream = new ObjectInputStream(new FileInputStream(RANGERING_FIL));
+            ArrayList<Spiller> list = (ArrayList<Spiller>)inputStream.readObject();
             
-            double poeng;
-            String navn;
-            while(sc.hasNextLine()){
-                navn = sc.next();
-                poeng = sc.nextDouble();
+            return list;
             
-            spillere.add(new Spiller(navn,poeng));
-                              
+            
+
+        } catch (FileNotFoundException e) {
+            System.out.println("[Last inn] FNF Error: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("[Last inn] IO Error: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("[Last inn] CNF Error: " + e.getMessage());
+        } finally {
+            try {
+                if (outputStream != null) {
+                    outputStream.flush();
+                    outputStream.close();
+                }
+            } catch (IOException e) {
+                System.out.println("[Last inn] IO Error: " + e.getMessage());
             }
-        sc.close();
-        }catch(Exception e){
-            
-             e.printStackTrace(System.out);
         }
+        return null;
+    }
+      
+     public void oppdaterRangering(Spiller nySpiller) {
+        try {
+            outputStream = new ObjectOutputStream(new FileOutputStream(TURNERING_FIL));
+            outputStream.writeObject(nySpiller);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("[Oppdatering] FNF Error: " + e.getMessage() + ", programmet lager ny fil!");
+        } catch (IOException e) {
+            System.out.println("[Oppdatering] IO Error: " + e.getMessage());
+        } finally {
+            try {
+                if (outputStream != null) {
+                    outputStream.flush(); 
+                    outputStream.close();
+                }
+            } catch (IOException e) {
+                System.out.println("[Oppdatering] Error: " + e.getMessage());
+            }
+        }
+    }
        
         
-        return spillere;
-        }
         
     
 }
