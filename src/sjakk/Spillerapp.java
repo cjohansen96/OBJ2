5c4ad1edb128f76ebdcc32af2c9a6bb71af73e40
Unknown <christofferjohansen12@gmail.com>
2019-05-06 10:11:07 +0200

    Mappe struktur

---
+++ src/sjakk/Spillerapp.java
@@ -0,0 +1,14 @@
+package sjakk;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Spillerapp extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+    
+}
