6c476c1c6f5eff154dfdb11f5398644e940f666b
Markus W.Byremo <markuswb77@gmail.com>
2019-05-06 11:42:19 +0200

    Ny spillApp fil

---
+++ src/sjakk/SpillerApp.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sjakk;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Markus,Thomas
+ */
+public class SpillerApp extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+       
+       
+        
+      
+        
+        primaryStage.setTitle("SpillerApp");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
