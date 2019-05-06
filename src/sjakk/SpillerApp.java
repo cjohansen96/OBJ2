b80b9ac106ea3140390208a6c0af1db439c730ad
Markus W.Byremo <markuswb77@gmail.com>
2019-05-06 12:55:27 +0200

    Oppdatering av GUI layout

--- src/sjakk/SpillerApp.java
+++ src/sjakk/SpillerApp.java
@@ -8,27 +8,68 @@ package sjakk;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
  *
- * @author Markus,Thomas
+ * @author Markus
  */
 public class SpillerApp extends Application {
+    
+     
+    private final static int WIDTH = 1000;
+    private final static int HEIGHT = 700;
+    
+    
+    BorderPane root;
+    StackPane window;
+    VBox sideMenu;
+    Scene sceneTable,sceneAn;
     
     @Override
     public void start(Stage primaryStage) {
         
+        
+       root = new BorderPane();
+       
+       window = new StackPane();
+       window.setBorder(new Border(
+            new BorderStroke(Color.BLUE, 
+            BorderStrokeStyle.SOLID,
+            CornerRadii.EMPTY,
+            BorderWidths.DEFAULT)));
+        
+       window.setAlignment(Pos.CENTER);
+       
+       sideMenu = new VBox();
+       sideMenu.setPadding(new Insets(60.0));
+       
+       
+       
+       
+       root.setLeft(sideMenu);
+       root.setCenter(window);
+       sceneTable = new Scene(root,WIDTH,HEIGHT);
        
        
         
       
         
         primaryStage.setTitle("SpillerApp");
-        primaryStage.setScene(scene);
+        primaryStage.setScene(sceneTable);
         primaryStage.show();
     }
 
